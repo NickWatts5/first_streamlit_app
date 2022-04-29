9addd5cac759cf21293565a03dfc2f1299102e1e
Nick Watts <nick@dialanexchange.com>
2022-04-29 15:39:57 +0100

    Update streamlit_app.py
    Breakfast menu.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My Mum\'s New Healthy Diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free Range Egg')
